a909d53e221946c55baba6ddb0efb087b966341c
Estivador <Estivador@9cc75e76-0158-11df-9af4-1984060cff7e>
2010-01-15 12:11:48 +0000

    Ajustes no formato do projeto.

--- jcgf/src/cgf/rmi/IPlayer.java
+++
@@ -1,9 +0,0 @@
-package cgf.rmi;
-
-import java.io.Serializable;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IPlayer extends /* Observer, */Remote, Serializable {
-	public void update(/* IPlayer */String sender, Object valor) throws RemoteException;
-}
--- jcgf/src/cgf/visao/ILayoutStrategy.java
+++
@@ -1,8 +0,0 @@
-package cgf.visao;
-
-import cgf.rmi.IPlayer;
-
-public interface ILayoutStrategy {
-	// public ILayoutStrategy getLayoutStrategy();
-	public void reorganiza(IPlayer modelo);
-}
